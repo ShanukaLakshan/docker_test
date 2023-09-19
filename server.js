e02d225a5710f1a01abeba71b673b765ee4d81b3
ShanukaLakshan <lakshants.20@uom.lk>
2023-09-19 17:11:29 +0530

    Add

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "Hello World" });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
