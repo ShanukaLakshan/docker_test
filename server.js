d2aa9501b7d217a99730fd70cf9d351cf0e8a280
ShanukaLakshan <lakshants.20@uom.lk>
2023-09-19 22:54:45 +0530

    test1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ status: 200, message: "Hello Shanu" });
+  res.json({ status: 200, message: "Hello Shanuka" });
 });
 
 app.listen(8080, () => {
